27d290014b3bd5b0d3f9169f1bfc9b466f096675
Aditya Patil <144269421+patiladitya92@users.noreply.github.com>
2025-05-13 17:33:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -248,8 +248,7 @@ def stop_camera():
     return "Camera stopped"
 
 # ✅ Render-compatible launch
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
 
